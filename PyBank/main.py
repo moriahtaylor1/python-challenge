9159d22e354399dfdaf748838382b0b0a60b60cf
Moriah Taylor <moriah.taylor@duke.edu>
2021-03-16 19:14:35 -0400

    simple calculations

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,3 +8,8 @@ budget_df = pd.read_csv("Resources/budget_data.csv")
 #convert to pandas dataframe
 budget_df = pd.DataFrame(budget_df)
 
+##total nums of months##
+num_months = len(budget_df)
+
+##net amount of profit/losses##
+net_total = budget_df["Profit/Losses"].sum()
