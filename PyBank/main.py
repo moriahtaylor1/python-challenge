93f654874b71244af9441aa7ee9e3158fb8cd284
Moriah Taylor <moriah.taylor@duke.edu>
2021-03-16 19:16:42 -0400

    get avg changes with while loop

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,3 +13,17 @@ num_months = len(budget_df)
 
 ##net amount of profit/losses##
 net_total = budget_df["Profit/Losses"].sum()
+
+##average of changes##
+list_changes = []    #create empty list
+i = 1                 #set start index
+stop = len(budget_df)  #set stop index
+
+#loop through data
+while i < stop:
+    change = budget_df["Profit/Losses"][i] - budget_df["Profit/Losses"][i-1]    #calculate change
+    list_changes.append(change)    #add value of change to list
+    i += 1    #next row
+
+avg_change = sum(list_changes) / len(list_changes)    #calculate average
+avg_change = round(avg_change, 2)    #format to 2 decimal places
