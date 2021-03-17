30bd11ab0111c5201062ac65159dd342f1cf8ecb
Moriah Taylor <moriah.taylor@duke.edu>
2021-03-16 20:09:45 -0400

    create analysis table in txt.file

--- PyBank/main.py
+++ PyBank/main.py
@@ -37,3 +37,17 @@ max_inc_date = max_inc_row.iloc[0,0]    #pull date from row
 max_dec = min(budget_df["Profit/Losses"])    #find greatest decrease
 max_dec_row = budget_df[budget_df["Profit/Losses"]==max_dec]    #get row
 max_dec_date = max_dec_row.iloc[0,0]    #pull date from row
+
+##output analysis table as .txt file##
+
+os.chdir('../PyBank/Analysis')
+
+with open('analysis.txt', 'w') as f:
+    #write in file
+    print('Financial Analysis', end='\n', file=f)
+    print('----------------------------', end='\n', file=f)
+    print(f'Total Months: {num_months}', end='\n', file=f)
+    print(f'Total: ${net_total}', end='\n', file=f)
+    print(f'Average Change: ${avg_change}', end='\n', file=f)
+    print(f'Greatest Increase in Profits: {max_inc_date} (${max_inc})', end='\n', file=f)
+    print(f'Greatest Decrease in Profits: {max_dec_date} (${max_dec})', file=f)
