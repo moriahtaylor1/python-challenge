6ce8b8a0bb2e2555bcb49689e0ecdbb9b5cc685a
Moriah Taylor <moriah.taylor@duke.edu>
2021-03-19 16:48:11 -0400

    vote counts

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import sys
 import os
+from collections import Counter
 
 #read in csv file
 election_df = pd.read_csv("Resources/election_data.csv")
@@ -23,4 +24,9 @@ li_perc = round(vote_perc[2], 2)
 otooley_perc = round(vote_perc[3], 2)
 
 ##vote per candidate##
-vote_count = election_df["Candidate"].value_counts()
+vote_count = Counter(election_df["Candidate"])
+
+#put vote counts into dictionary
+max_votes=max(vote_count.values())     #put max num of votes in a variable
+winners =[i for i in vote_count.keys() if vote_count[i]==max_votes]    #find name of winner
+winner = winners[0]    #abstract name of winner from list
